8d80a74eed3355849b0a2b7dc8bdd71fdbd2b8c1
Paul Duguet <paul.duguet@rvu.co.uk>
2021-07-29 16:47:29 +0100

    CRUD support

--- segment/destination_filters.go
+++ segment/destination_filters.go
@@ -8,9 +8,11 @@ import (
 	"github.com/pkg/errors"
 )
 
+var updateMask = newUpdateMask("if", "actions", "title", "description", "enabled")
+
 // ListDestinations returns all destinations for a source
 func (c *Client) ListDestinationFilters(srcName string, destinationName string) ([]DestinationFilter, error) {
-	var d DestinationFilters
+	var d destinationFiltersListResponse
 	data, err := c.doRequest(http.MethodGet,
 		fmt.Sprintf("%s/%s/%s/%s/%s/%s/%s",
 			WorkspacesEndpoint, c.workspace, SourceEndpoint, srcName, DestinationEndpoint, destinationName, DestinationFiltersEndpoint),
@@ -24,4 +26,64 @@ func (c *Client) ListDestinationFilters(srcName string, destinationName string) 
 	}
 
 	return d.Filters, nil
+}
+
+func (c *Client) CreateDestinationFilter(srcName string, destinationName string, filter DestinationFilter) (*DestinationFilter, error) {
+	data, err := c.doRequest(http.MethodPost,
+		fmt.Sprintf("%s/%s/%s/%s/%s/%s/%s",
+			WorkspacesEndpoint, c.workspace, SourceEndpoint, srcName, DestinationEndpoint, destinationName, DestinationFiltersEndpoint),
+		destinationFilterCRURequest{Filter: filter, UpdateMask: updateMask})
+	if err != nil {
+		return nil, err
+	}
+
+	var result DestinationFilter
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal filter")
+	}
+
+	return &result, nil
+}
+
+func (c *Client) UpdateDestinationFilter(srcName string, destinationName string, filter DestinationFilter) (*DestinationFilter, error) {
+	data, err := c.doRequest(http.MethodPatch, filter.Name, destinationFilterCRURequest{Filter: filter, UpdateMask: updateMask})
+	if err != nil {
+		return nil, err
+	}
+
+	var result DestinationFilter
+	err = json.Unmarshal(data, &result)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal filter")
+	}
+
+	return &result, nil
+}
+
+func (c *Client) GetDestinationFilter(srcName string, destinationName string, filterId string) (*DestinationFilter, error) {
+	data, err := c.doRequest(http.MethodGet,
+		fmt.Sprintf("%s/%s/%s/%s/%s/%s/%s/%s",
+			WorkspacesEndpoint, c.workspace, SourceEndpoint, srcName, DestinationEndpoint, destinationName, DestinationFiltersEndpoint, filterId),
+		nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var d destinationFilterCRURequest
+	err = json.Unmarshal(data, &d)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal filter")
+	}
+
+	return &d.Filter, nil
+}
+
+func (c *Client) DeleteDestinationFilter(srcName string, destinationName string, filterId string) error {
+	_, err := c.doRequest(http.MethodDelete,
+		fmt.Sprintf("%s/%s/%s/%s/%s/%s/%s/%s",
+			WorkspacesEndpoint, c.workspace, SourceEndpoint, srcName, DestinationEndpoint, destinationName, DestinationFiltersEndpoint, filterId),
+		nil)
+
+	return err
 }
